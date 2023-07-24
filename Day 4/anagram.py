0415cfb71348ec61ff205d66713b45a82310f79d
Anjali N V <anjaliprathees.nv@gmail.com>
2023-07-24 22:42:21 +0530

    day4 commit

---
+++ Day 4/anagram.py
@@ -0,0 +1,16 @@
+#This python program is to check whether a given string is anagram or not
+def isAnagram(s,t):
+    if len(s) != len(t):
+        return False
+    countS , countT = {},{}
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i] , 0)
+        countT[t[i]] = 1 + countT.get(t[i] , 0)
+    for c in countS: 
+        if countS[c]!= countT.get(c,0):
+            return False
+    return True
+s = "anagram"
+t = "nagaram"
+result = isAnagram(s,t)
+print(result)
